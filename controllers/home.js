7da338a8487fd93d0c7d55bab72d2b1a3e7caaf1
Natalie Blumen <natalie.blumen@gmail.com>
2025-01-23 10:26:27 -0500

    jan23

--- controllers/home.js
+++ controllers/home.js
@@ -1,6 +1,17 @@
+const Listing = require("../models/Listing");
+const User = require("../models/User");
+const Organization = require("../models/Organization");
+
 module.exports = {
-  getIndex: (req, res) => {
-    res.render("index.ejs");
+  getIndex: async (req, res) => {
+    try{
+      const listings = await Listing.find({});
+      res.render("index.ejs", { listing: listings, user: req.user });
+    } catch(err) {
+      console.log(err);
+      res.status(500).send("Server error");
+    }
+    
   },
   getAbout: (req, res) => {
     res.render("about.ejs", { section: "about"});
